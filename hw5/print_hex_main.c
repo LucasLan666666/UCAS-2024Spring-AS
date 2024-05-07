01c4fbf4febe1a98c27d2a76aca721e304951f38
Yuzhou Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-05-07 17:16:39 +0800

    hw5

---
+++ hw5/print_hex_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern void print_hex(int num);
+
+int
+main(int argc, char** argv)
+{
+    print_hex(0x0812abcd);
+    // get 0x812abcd
+
+    print_hex(0x80001234);
+    // get -0x7fffedcc
+
+    return 0;
+}
