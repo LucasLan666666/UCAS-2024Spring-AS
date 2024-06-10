1933fc3c72ec34fe14ed9ba6f783b69533888662
Lucas Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-06-11 04:51:48 +0800

    hw9

--- hw9/hex2dec_main.c
+++ hw9/hex2dec_main.c
@@ -6,14 +6,14 @@ extern void hex2dec_gold(uint32_t n);
 
 int
 main(int argc, char** argv) {
-    printf("Standard output: ");
+    printf("Standard output:\n");
     hex2dec_gold(0x9812abcd);
-    printf("Your output: ");
+    printf("Your output:\n");
     hex2dec(0x9812abcd);
 
-    printf("Standard output: ");
+    printf("Standard output:\n");
     hex2dec_gold(0x0fff);
-    printf("Your output: ");
+    printf("Your output:\n");
     hex2dec(0x0fff);
 
     return 0;
