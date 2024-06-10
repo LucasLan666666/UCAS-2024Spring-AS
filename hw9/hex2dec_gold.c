0d605f96b30f55f48f0ed8ffae22c327e1a68ac6
Lucas Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-06-10 15:54:55 +0800

    add abi

---
+++ hw9/hex2dec_gold.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void
+hex2dec_gold(uint32_t n)
+{
+    printf("%u\n", n);
+}
---
+++ hw9/hex2dec_main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern void hex2dec(uint32_t n);
+extern void hex2dec_gold(uint32_t n);
+
+int
+main(int argc, char** argv) {
+    printf("Standard output: ");
+    hex2dec_gold(0x9812abcd);
+    printf("Your output: ");
+    hex2dec(0x9812abcd);
+
+    printf("Standard output: ");
+    hex2dec_gold(0x0fff);
+    printf("Your output: ");
+    hex2dec(0x0fff);
+
+    return 0;
+}
